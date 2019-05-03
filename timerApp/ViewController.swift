e8d12710f275635781a640b801934e3e7aca16bc
nanomish <michael.kushniriov@gmail.com>
2019-05-03 13:48:14 +0300

    Initial Commit

---
+++ timerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timerApp
+//
+//  Created by Michael Kushniriov on 03/05/2019.
+//  Copyright © 2019 Michael Kushniriov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
